3e4d038a6f49637703160cc74b31df801462ef89
Maurizio Pietrantuono <maurizio.pietrantuon@Mac2210.local>
2018-04-17 14:03:48 +0100

    Test commit

--- app/src/main/java/com/pppp/appcentercitestapp/MainActivity.kt
+++ app/src/main/java/com/pppp/appcentercitestapp/MainActivity.kt
@@ -12,6 +12,7 @@ class MainActivity : AppCompatActivity() {
     setContentView(R.layout.activity_main)
     findViewById<Button>(R.id.button).setOnClickListener {
       findViewById<TextView>(R.id.text).setText("OK")
+      //bla bla
     }
   }
 }
