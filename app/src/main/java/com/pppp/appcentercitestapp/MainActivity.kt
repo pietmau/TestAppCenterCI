c792fe8f519e443114e6bc063c822f3c0713f24b
Maurizio Pietrantuono <maurizio.pietrantuon@Mac2210.local>
2018-04-17 13:21:22 +0100

    First commit

---
+++ app/src/main/java/com/pppp/appcentercitestapp/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.pppp.appcentercitestapp
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+    findViewById<Button>(R.id.button).setOnClickListener {
+      findViewById<TextView>(R.id.text).setText("OK")
+    }
+  }
+}
